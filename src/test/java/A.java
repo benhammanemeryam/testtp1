349ecbbe57f54cf8edafdc3cf67f1b364664373b
meryambenhammane <meryambenhammane532@gmail.com>
2022-09-29 08:02:58 +0200

    Initial commit

---
+++ src/test/java/A.java
@@ -0,0 +1,12 @@
+public class A {
+    public static void main(String[] args){
+        int monAge = 21;
+        boolean estMageur = monAge>=18;
+        if(estMageur){
+            System.out.println("je suis majeur ");
+        }
+        else {
+            System.out.println("je suis mineur");
+}
+}
+}
